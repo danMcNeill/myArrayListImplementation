fc03916a6fac957a86e4d99d51d2d3adf3df8c14
Daniel McNeill <dmcneil2@cs.binghamton.edu>
2017-09-03 23:55:38 -0400

    some functions of list object no comments yet

--- myArrayList/src/myArrayList/MyArrayList.java
+++ myArrayList/src/myArrayList/MyArrayList.java
@@ -1,5 +1,41 @@
 public class MyArrayList {
 	
+	private int[] intArray;
+	private int arraySize;
+
+	public MyArrayList() {
+		this.intArray = new int[50];
+		this.arraySize = 50;
+	}
+	
+	public String toString() {
+
+	}
+
+	public int size() {
+		return this.arraySize;
+	}
+
+	public void setArraySize(int newSize) {
+		this.arraySize = newSize;
+	}
+
+	public int sum() {
+		int sum = 0;
+		for(int i=0; i<arraySize; i++) {
+			sum += intArray[i];
+		}
+		return sum;
+	}
+
+	public int indexOf(int value) {
+		int index = -1;
+		for(int i=0; i<arraySize; i++) {
+			if(value == intArray[i])
+				index = i;
+		return index;
+	}
+}
 
 
 
